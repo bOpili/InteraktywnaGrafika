20fc370fd7eaa1c740fa956224270ee1a62609b0
bOp <161240137+bOpili@users.noreply.github.com>
2024-04-03 21:27:50 +0200

    v6

--- script.js
+++ script.js
@@ -8,3 +8,32 @@ function zmienKontrast(){
         styleLink.setAttribute("href", "style.css")
     }
 }
+
+function naSrodek(photo){
+    var r = document.querySelector(':root');
+    var ae = document.getElementsByClassName("centeredImage");
+    var ae2 = document.getElementsByClassName("photoBack");
+    
+    Array.prototype.forEach.call(ae2, function(el) {
+        el.className = "photo";
+    });
+    
+    Array.prototype.forEach.call(ae, function(el) {
+        el.className = "photoBack";
+    });
+
+    
+    photo.className = 'centeredImage';
+    
+    var gallery = document.getElementById('gallery');
+    
+    var photoRect = photo.getBoundingClientRect();
+    
+    var rect = gallery.getBoundingClientRect();
+    
+    
+    var oy = rect.bottom - photoRect.bottom;
+    var ox = (rect.right/2) - photo.right;
+    r.style.setProperty('--oy', oy.toString()+'px');
+    r.style.setProperty('--ox', ox.toString()+'px');
+}
