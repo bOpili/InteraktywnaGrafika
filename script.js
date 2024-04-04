81760b5220a95f5f4b2827ac29b02a737b4f93e3
bOp <161240137+bOpili@users.noreply.github.com>
2024-04-04 13:45:17 +0200

    v7

--- script.js
+++ script.js
@@ -1,3 +1,62 @@
+window.onload = function() {
+    var canvas = document.getElementById("myCanvas");
+    var context = canvas.getContext("2d");
+
+    var szer = 50;
+    var wys = 100;
+
+    
+    context.beginPath();
+    context.lineWidth = 2;
+    context.strokeStyle = "red";
+    context.moveTo(0,0);
+
+    for (var i = 1; i<10; i+=2){
+        context.lineTo(i*szer,0);
+        context.lineTo(i*szer,wys);
+        context.lineTo((i+1)*szer,wys);
+        context.lineTo((i+1)*szer,0);
+    }
+
+    context.stroke();
+
+    // context.beginPath();
+    // context.lineWidth = 2;
+    // context.strokeStyle = "blue";
+    // context.moveTo(150,0);
+    // context.beginPath();
+    // context.arc(150, 150, 75, 0, 1*Math.PI, false)
+    // context.stroke();
+
+    context.beginPath();
+    var tab = Array(10,30,50,120);
+    var colors = Array ("blue", "red", "yellow", "green");
+    context.lineWidth = 2;
+    context.strokeStyle = colors[0];
+    // context.moveTo(150,150);
+    var rad1 = 0;
+    var rad2 = (360*tab[0]/100)*0.0174532925;
+    context.arc(150,150, 20, rad1, rad2, false);
+    context.stroke();
+    for (var i = 1; i<tab.length;i++){
+        context.beginPath();
+        context.lineWidth = 2;
+        context.strokeStyle = colors[i];
+        var rad1 = rad2;
+        var rad2 = (360*tab[i]/100)*0.0174532925;
+        context.arc(150,150, 20, rad1, rad2, false);
+        context.stroke();
+        console.log("rad1="+rad1.toString() + ", rad2="+rad2.toString());
+    }
+    
+
+
+    context.stroke();
+    
+
+
+}
+
 function zmienKontrast(){
     var styleLink = document.getElementById("style");
     var currentStyle = styleLink.getAttribute("href");
