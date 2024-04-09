14e905b352f4fd9f0808f04c08c7eda1f3046219
bOp <161240137+bOpili@users.noreply.github.com>
2024-04-09 20:00:41 +0200

    lab 4 bez lab 3

--- script.js
+++ script.js
@@ -17,55 +17,145 @@ window.onload = function() {
         context.lineTo((i+1)*szer,wys);
         context.lineTo((i+1)*szer,0);
     }
-
     context.stroke();
+    context.closePath();
 
-    // context.beginPath();
-    // context.lineWidth = 2;
-    // context.strokeStyle = "blue";
-    // context.moveTo(150,0);
-    // context.beginPath();
-    // context.arc(150, 150, 75, 0, 1*Math.PI, false)
-    // context.stroke();
+    //context.stroke();
 
     context.beginPath();
+    
     var tab = Array(10,30,50,120);
-    var colors = Array ("blue", "red", "yellow", "green");
+    var full = 0;
+    tab.forEach(element => {
+        full += element;
+    });
+    var colors = Array ("#ccccff", "#ffcccc", "#ffffcc", "#ccffcc");
+    var colors2 = Array ("#0000ff", "#ff0000", "#ffff00", "#00ff00");
     context.lineWidth = 2;
     context.strokeStyle = colors[0];
-    // context.moveTo(150,150);
+    var grad = context.createLinearGradient(150,300,250,300);
+    grad.addColorStop(0, colors[0]);
+    grad.addColorStop(1, colors2[0]);
+    context.lineTo(150,300);
     var rad1 = 0;
-    var rad2 = (360*tab[0]/100)*0.0174532925;
-    context.arc(150,150, 20, rad1, rad2, false);
-    context.stroke();
+    var perc = tab[0]/full;
+    var rad2 = rad1+(360*perc)*0.0174532925;
+    console.log("rad1 "+rad1.toString()+ ", rad2="+rad2.toString());
+    context.arc(150,300, 100, rad1, rad2, false);
+    context.lineTo(150,300);
+    context.fillStyle = grad;
+    context.fill();
+    //context.stroke();
     for (var i = 1; i<tab.length;i++){
         context.beginPath();
         context.lineWidth = 2;
         context.strokeStyle = colors[i];
+        var perc = tab[i]/full;
         var rad1 = rad2;
-        var rad2 = (360*tab[i]/100)*0.0174532925;
-        context.arc(150,150, 20, rad1, rad2, false);
-        context.stroke();
-        console.log("rad1="+rad1.toString() + ", rad2="+rad2.toString());
+        var rad2 = rad1+(360*perc)*0.0174532925;
+        if(rad2<1.57){
+            grad = context.createLinearGradient(150,300,250,300);
+        }else if(rad2<3.14){
+            grad = context.createLinearGradient(150,300,150,400);
+        }else if(rad2<4.6472){
+            grad = context.createLinearGradient(150,300,50,150);
+        }else{
+            grad = context.createLinearGradient(150,300,150,200);
+        }
+
+        grad.addColorStop(0, colors[i]);
+        grad.addColorStop(1, colors2[i]);
+        context.arc(150,300, 100, rad1, rad2, false);
+        context.lineTo(150,300);
+        context.fillStyle = grad;
+        context.fill();
+        //context.stroke();
+        //console.log("rad1 "+rad1.toString()+ ", rad2="+rad2.toString());
     }
-    
-
-
     context.stroke();
+    context.closePath();
     
-
-
+    //context.stroke();
+    
+    
+    context.beginPath();
+    grad = context.createLinearGradient(50,650,550,600);
+    grad.addColorStop(0, "#ffcccc");
+    grad.addColorStop(1, "#ff0000");
+    context.lineWidth = 2;
+    context.strokeStyle = grad;
+    context.moveTo(50,500);
+    context.lineTo(450,500);
+    context.arcTo(500,500,500,550,50);
+    context.lineTo(500,650);
+    context.arcTo(500,700,450,700,50);
+    context.lineTo(50,700);
+    context.arcTo(0,700,0,650,50);
+    context.lineTo(0,550);
+    context.arcTo(0,500,50,500,50);
+    
+    context.fillStyle = grad;
+    context.fill();
+    context.stroke();
+    context.closePath();
+    //context.stroke();
+    
+    
+    context.beginPath();
+    context.moveTo(200, 800);
+    var koloZ = { x: 200, y: 800, r1:70, r2:80 };
+    var ile = 84; //liczba zębów
+    var r = koloZ.r1;
+    context.lineWidth = 2;
+    context.strokeStyle = 'blue';
+    
+    for (var i = 0; i < ile; i++) {
+        var alpha = ((Math.PI * 2) / ile) * (i); //aktualny kąt
+        if (i % 2 == 0) {
+            if (r == koloZ.r1)
+                r = koloZ.r2;
+            else
+                r = koloZ.r1;
+        }
+        var x = (r * Math.sin(alpha)) + koloZ.x;
+        var y = (r * Math.cos(alpha)) + koloZ.y;
+        
+        if (i == 0)
+            context.moveTo(x, y);
+        else
+            context.lineTo(x, y);
+    }
+        
+        context.fillStyle = 'blue';
+        context.fill();
+        context.closePath();
+        context.stroke();
+        context.globalCompositeOperation ="destination-out";
+        
+    for (var i = 0; i < 5; i++){
+        context.beginPath();
+        var alpha = ((Math.PI * 2) / 5) * (i);
+        var x = (r/2 * Math.sin(alpha)) + koloZ.x;
+        var y = (r/2 * Math.cos(alpha)) + koloZ.y;
+        context.lineWidth = 2;
+        context.strokeStyle = 'red';
+        context.arc(x, y, 10, 0, 2 * Math.PI);
+        context.fillStyle = 'red';
+        context.fill();
+        context.stroke();
+        context.closePath();
+    }
 }
 
 function zmienKontrast(){
-    var styleLink = document.getElementById("style");
-    var currentStyle = styleLink.getAttribute("href");
+    // var styleLink = document.getElementById("style");
+    // var currentStyle = styleLink.getAttribute("href");
 
-    if (currentStyle == "style.css"){
-        styleLink.setAttribute("href", "styleHC.css")
-    }else{
-        styleLink.setAttribute("href", "style.css")
-    }
+    // if (currentStyle == "style.css"){
+    //     styleLink.setAttribute("href", "styleHC.css")
+    // }else{
+    //     styleLink.setAttribute("href", "style.css")
+    // }
 }
 
 function naSrodek(photo){
